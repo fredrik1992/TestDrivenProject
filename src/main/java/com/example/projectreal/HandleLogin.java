8170bb6099e8a97393980fed8cfe6bb4d4348dd6
fredrik1992 <klosansa1@.com>
2021-11-11 23:54:42 +0100

    1.21 simple register check will be used later

--- src/main/java/com/example/projectreal/HandleLogin.java
+++ src/main/java/com/example/projectreal/HandleLogin.java
@@ -1,16 +1,25 @@
 package com.example.projectreal;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class HandleLogin {
+    private Map<String,String> existingUsers = new HashMap<>();
 
-    private Map<String,String> existingUsers = Map.ofEntries(
-            Map.entry("fredrik","lady2005"),
-            Map.entry("test","test2")
-    );
 
     public boolean login(String uname,String password) {
         if(existingUsers.get(uname) == password) return true;
+        return false;
+    }
+
+    public boolean registerNewAccount(String username, String password) {
+        //needs to be encrypted later
+        if (!existingUsers.containsKey(username)){
+            existingUsers.put(username,password);
+            System.out.print(existingUsers.size());
+
+            return true;
+        }
         return false;
     }
 }
--- src/test/java/com/example/projectreal/HandleLoginTest.java
+++ src/test/java/com/example/projectreal/HandleLoginTest.java
@@ -10,11 +10,15 @@ public class HandleLoginTest {
     @BeforeEach
     void setUp() {
         handleLogin = new HandleLogin();
+        handleLogin.registerNewAccount("fredrik","lady2005");
     }
 
     @Test
     void test_login_authentication() {
-        handleLogin = new HandleLogin();
         assertTrue(handleLogin.login("fredrik","lady2005")); //simulates some1 entering username the
+    }
+    @Test
+    void test_register_new_account() {
+        assertTrue(handleLogin.registerNewAccount("fredriks","lady2005")); //Checks if the new user can login
     }
 }
